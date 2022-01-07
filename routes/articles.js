0a6a36721c7e68c2de2b0132c62c0815b07286e1
Aziz-ba <76202670+Aziz-ba@users.noreply.github.com>
2022-01-07 16:43:32 +0100

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -1,47 +1,59 @@
-const express = require("express");
+const { Router } = require("express");
 const Articles = require("../models/Articles");
-const router = express.Router();
-
+const pool = require('../lib/db.js');
+const bcrypt = require('bcrypt');
+const router = new Router();
 router.get("/articles", (req, res) => {
   const criteria = req.query;
   Articles.findAll({
+    attributes: ['authorId', 'firstname','lastname', , 'createdAt', 'updatedAt','content','title','tags','delatedAt','isAdmin'],
     where: criteria,
-  }).then((users) => res.json(users));
-});
-
-router.post("/articles", (req, res) => {
-  Articles.create(req.body).then((user) => res.status(201).json(user));
-});
-
-router.get("/articles/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  Articles.findByPk(id).then((user) => {
-    if (!user) res.sendStatus(404);
-    else res.json(user);
+  }).then((users) => {
+    res.json(users); 
   });
 });
 
-router.delete("/articles/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+
+router.post("/users", (req, res) => {
+    Articles.create(req.body).then((articles) => {
+    res.status(201).json(articles);
+  });
+});
+
+router.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  Articles.findByPk(id,{
+    attributes: ['authorId', 'firstname','lastname', , 'createdAt', 'updatedAt','content','title','tags','delatedAt','isAdmin'],
+  }).then((articles) => {
+    if (!articles) res.sendStatus(404);
+    else res.json(articles);
+
+  });
+});
+
+router.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  Articles.update(req.body, {
+    where: { id: id },
+  }).then(([nbUpdated]) => {
+    if (!nbUpdated) res.sendStatus(404);
+    else Articles.findByPk(id).then((articles) => res.json(articles));
+  });
+});
+
+router.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
   Articles.destroy({
-    where: {
-      id: id,
-    },
+    where: { id: id },
   }).then((nbDeleted) => {
     if (!nbDeleted) res.sendStatus(404);
     else res.sendStatus(204);
   });
 });
 
-router.put("/articles/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  Articles.update(req.body, {
-    where: {
-      id: id,
-    },
-  }).then(([nbUpdated]) => {
-    if (!nbUpdated) res.sendStatus(404);
-    else Articles.findByPk(id).then((user) => res.json(user));
+router.post('/register', async function (req, res) {
+    Articles.create(req.body).then((articles) => {
+    res.status(201).json(articles);
   });
 });
 
